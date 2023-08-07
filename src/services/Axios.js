ff9d90ef133832c2d5910b17b36077da91a22e5f
kail48 <kailash48rai@gmail.com>
2023-08-07 22:56:21 +0545

    added useaxios hook

---
+++ src/services/Axios.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+axios.defaults.baseURL = ' http://127.0.0.1:5000';
+
+const useAxios = ({ url, method, body = null, headers = null }) => {
+    const navigate = useNavigate();
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState('');
+    const [loading, setloading] = useState(true);
+
+    const fetchData = () => {
+        axios[method](url, JSON.parse(headers), JSON.parse(body))
+            .then((res) => {
+                console.log("res",res)
+                setResponse(res.data);
+            })
+            .catch((err) => {
+                if(err.message==="Network Error"){
+                    navigate('/server-not-found')
+                }
+                setError(err);
+            })
+            .finally(() => {
+                setloading(false);
+            });
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [method, url, body, headers]);
+
+    return { response, error, loading };
+};
+
+export default useAxios;
