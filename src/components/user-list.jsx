4f47dc4116dbc3279b02b9ab56209883b8d0575d
kail48 <kailash48rai@gmail.com>
2023-08-08 15:57:40 +0545

    added api call function and implemented pagination successfully

--- src/components/user-list.jsx
+++ src/components/user-list.jsx
@@ -1,16 +1,69 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Pagination } from '@mantine/core';
+import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
 export default function UserList() {
+  const navigate=useNavigate()
     const [activePage, setPage] = useState(1);
-    const data=[]
-  return (
-    <div className='w-full'>
-        <div className='h-72 w-full'>
+    const [users,setUsers]=useState(null)
+    const [totalPage,setTotalPage]=useState(0)
 
-        </div>
+    
+    const fetchUsers=(token)=>{
+      let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: `http://127.0.0.1:5000/users?page=${activePage}`,
+        headers: { 
+          'Authorization': `Bearer ${token}`
+        }
+      };
+      axios.request(config)
+    .then((response) => {
+      setUsers(response.data.users)
+      setTotalPage(response.data.total_pages-1)
+      
+    })
+    .catch((error) => {
+      console.log(error.response.data);
+      if(error.response){
+        if(error.response.data.msg){
+          if(error.response.data.msg==="Token has expired"){
+            toast.error("session has expired login again! please login again")
+            setTimeout(() => {
+              navigate('/login');
+            }, 1500);
+          }
+        }
+      }
+    });
+    
+    }
+    useEffect(() => {
+      let token=null
+      if(!localStorage.getItem("token")){
+        navigate('/login')
+    }
+    else{
+        token=localStorage.getItem("token")
+        fetchUsers(token)
+    }
+
+  }, [activePage]);
+  return (
+    <div className='w-full h-96 flex flex-col justify-between px-4 py-5'>
+      <Toaster/>
+       <div className='w-full overflow-y-scroll'>
+        { users?
+          users.map((user,index)=>(
+            <div key={index}>{user.first_name}</div>
+          )):<div>loading</div>
+        }
+       </div>
        <div className='px-2 py-1'> 
-       <Pagination value={activePage} onChange={setPage} total={10} color="dark" size="sm" />
+       <Pagination value={activePage} onChange={setPage} total={totalPage} color="dark" size="sm" />
        </div>
     </div>
   )
