25b7d847b52dacb44d8f7aa33cac6dbf48981eb9
kail48 <kailash48rai@gmail.com>
2023-08-09 13:25:56 +0545

    added function to refresh list

--- src/components/artistTab.jsx
+++ src/components/artistTab.jsx
@@ -1,20 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import ArtistList from './artistList';
 import { useDisclosure } from '@mantine/hooks';
 import { Drawer} from '@mantine/core';
 import ArtistForm from './createArtist';
 import toast, { Toaster } from "react-hot-toast";
 import FileDownloader from './csvDownloader';
+import FileUploader from './csvUploader';
 export default function ArtistTab() {
 
 const [activeArtistTab,setActiveArtistTab]=useState("artist-list")
 const [opened, { open, close }] = useDisclosure(false);
+const [refreshToggle,setRefreshToggle]=useState(false)
 const pages={
     //this acts as store for tabs, teh setTab prop is callback funtion to set tabs
     "artist-list":<ArtistList setTab={setActiveArtistTab}/>,
 
 }
-
+const refreshTab=()=>setRefreshToggle(!refreshToggle)
+useEffect(() => {
+  
+}, [refreshToggle]);
   return (
     <div className="w-full h-full overflow-y-scroll my-8 ">
          <h1 className='text-center md:text-xl font-bold'>Artists</h1>
@@ -23,10 +28,10 @@ const pages={
      {pages[activeArtistTab]}
     </div>
     <Toaster/>
-    <div className='flex justify-end py-6 pr-10 gap-x-2'>
-    <button  onClick={open} className="py-2 px-2 bg-black text-white rounded-md hover:bg-gray-400 hover:text-black">Add New Artist</button>
+    <div className='flex justify-evenly py-6 pr-10 gap-x-6 mt-6'>
+    <button  onClick={open} className="py-3 px-2 bg-black text-white rounded-md hover:bg-gray-400 hover:text-black">Add New Artist</button>
     <FileDownloader/>
-    <button  onClick={()=>{}} className="py-2 px-2 bg-black text-white rounded-md hover:bg-gray-400 hover:text-black">Import From Csv</button>
+    <FileUploader refreshTab={refreshTab}/>
     </div>
     <Drawer opened={opened} onClose={close} title="">
         <ArtistForm/>
